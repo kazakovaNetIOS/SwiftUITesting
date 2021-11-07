e0cf2688e63c50d3ae8f3dc624b477724c764250
Kazakova Natalia Vladimirovna <natkazakova@ozon.ru>
2021-11-07 17:03:07 +0300

    Initial Commit

---
+++ SwiftUITesting/SwiftUITestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITestingApp.swift
+//  SwiftUITesting
+//
+//  Created by Kazakova Nataliya on 07.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
